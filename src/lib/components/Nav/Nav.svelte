6ddaebceca8ba0818007dc11c28525519ea81861
DevWaterdrop <82045791+DevWaterdrop@users.noreply.github.com>
2022-02-03 20:47:36 +0200

    Style: change Nav

--- src/lib/components/Nav/Nav.svelte
+++ src/lib/components/Nav/Nav.svelte
@@ -4,8 +4,15 @@
 
 <nav class="flex justify-between mb-8 items-center">
 	<div class="flex flex-col gap-1">
-		<h1 class="text-3xl font-bold">Greetings</h1>
-		<span class="text-xs">Supported by Halo lab</span>
+		<h1 class="text-3xl font-bold">Performance Three.js</h1>
+		<span class="text-xs"
+			>Supported by <a
+				class="transition border-b border-transparent hover:border-black hover:dark:border-white"
+				href="https://www.halo-lab.com"
+				target="_blank"
+				rel="noopener noreferrer">Halo lab</a
+			></span
+		>
 	</div>
 	<ThemeSwitch />
 </nav>
